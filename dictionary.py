d4a757bad18f0d73784bd5fdf6a9bea2c0128341
mcshow5678 <j21200209@gmail.com>
2024-12-05 00:25:06 +0100

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+dictionary = {
+    'bie' : 'come',
+    'ahu' : 'body',
+    'biko' : 'please',
+    'ala eka' : 'ankle',
+    'auz' : 'back',
+    'akwara' : 'vein',
+    'afo' : 'belly',
+}
+
+word = input('enter igbo word: ')
+print(dictionary[word])
